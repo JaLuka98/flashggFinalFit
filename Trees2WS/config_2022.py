3fc1adef2905b35357a0da66d0d44366e9fbf0a1
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2024-04-24 16:38:09 +0200

    Revert "Merge branch 'dev_ROOT_622' into dev_higgsdnafinalfit"
    This reverts commit c2e75972fd5b586c29a082680d3b3c1bd4c6a995, reversing
    changes made to 09aa9893d738fe6a11d57e2c8a2a4e969fa563c5.

--- Trees2WS/config_2022.py
+++ Trees2WS/config_2022.py
@@ -12,7 +12,6 @@ trees2wsCfg = {
   'notagVars':[], # Vars to add to NOTAG RooDataset
   'systematicsVars':["CMS_hgg_mass","weight","fiducialGeometricFlag"], # Variables to add to sytematic RooDataHists
   'theoryWeightContainers':{'weight_LHEPdf': 101, 'weight_LHEScale': 9},
-  #'theoryWeightContainers':{},
 
   # List of systematics: use string YEAR for year-dependent systematics
   'systematics':["Scale", "Smearing", "energyErrShift"],
