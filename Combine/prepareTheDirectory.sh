cf8704d94ea681783daa7c2e305a10946582d807
Nico Haerringer <n.haerringer@bluewin.ch>
2024-04-29 18:46:06 +0200

    Generalize prepareTheDirectory.sh script to work with different observables.

--- Combine/prepareTheDirectory.sh
+++ Combine/prepareTheDirectory.sh
@@ -1,28 +1,33 @@
-mkdir Models
-mkdir Models/signal
-mkdir Models/background
-cp ../Background/outdir_earlyAnalysis/CMS-HGG*.root ./Models/background/
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <variable, eg. pt>"
+  exit 1
+fi
+
+differential_variable="$1"
+
+
+mkdir "Models_${differential_variable}"
+mkdir "Models_${differential_variable}/signal"
+mkdir "Models_${differential_variable}/background"
+cp "../Background/outdir_earlyAnalysis_${differential_variable}/CMS-HGG*.root ./Models_${differential_variable}/background/"
 #cp ../Background/outdir_earlyAnalysis_2022preEE/CMS-HGG*.root ./Models/background/
 # Comment this renaming stuff in/out as needed, I am not totally sure about these year suffixes
 #for file in Models/background/*resolution.root; do mv "$file" "${file%.root}_2022preEE.root"; done
 #cp ../Background/outdir_earlyAnalysis_2022postEE/CMS-HGG*.root ./Models/background/
 #for file in Models/background/*resolution.root; do mv "$file" "${file%.root}_2022postEE.root"; done
-cp ../Signal/outdir_packaged/CMS-HGG*.root ./Models/signal/
+cp "../Signal/outdir_packaged_${differential_variable}/CMS-HGG*.root ./Models_${differential_variable}/signal/"
 # See above
-for file in Models/signal/*_2022.root; do
+for file in Models_${differential_variable}/signal/*_2022.root; do
     if [ -f "$file" ]; then
         new_name="${file%_2022.root}.root"
         mv "$file" "$new_name"
         echo "Renamed $file to $new_name"
     fi
 done
-if [ -e ../Datacard/Datacard_cleaned.txt ]; then
-    cp ../Datacard/Datacard_cleaned.txt Datacard.txt
+if [ -e ../Datacard/Datacard_${differential_variable}_cleaned.txt ]; then
+    cp "../Datacard/Datacard_${differential_variable}_cleaned.txt Datacard_${differential_variable}.txt"
 else
-    cp ../Datacard/Datacard.txt Datacard.txt
+    cp "../Datacard/Datacard_${differential_variable}.txt Datacard_${differential_variable}.txt"
 fi
-
-# Note that you might need to manually rename some paths in the datacard depending on the settings of the year 2022
-# Currently: Add 2022 to the background shapes, should be: 2022_13TeV_bkgshape
-# Uncapitalise Scales and Smearing nuisance parameters
-# pdfindex_best_resolution_13TeV --> pdfindex_best_resolution_2022_13TeV et. al.
