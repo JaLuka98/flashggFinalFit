3e500a8d6dec71147a78e3a13ba7db02b0b3fbb4
Nico Haerringer <n.haerringer@bluewin.ch>
2024-06-10 18:12:30 +0200

    Added prefix option in `multi_submit.sh` file.

--- Combine/multi_submit.sh
+++ Combine/multi_submit.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Check if differential_variable is provided as an argument
-# Check if both variables are provided as arguments
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <differential_variable> <sub_dir>"
+# Check if the correct number of arguments are provided
+if [ $# -lt 2 ] || [ $# -gt 3 ]; then
+  echo "Usage: $0 <differential_variable> <sub_dir> [prefix]"
   exit 1
 fi
+
 # Assign the provided argument to differential_variable
 differential_variable="$1"
 sub_dir="$2"
+prefix="$3"
 
 # Assuming your Condor submission files are located in a directory named "condor_files"
 condor_dir="./runFits_${differential_variable}/${sub_dir}/"
@@ -24,6 +25,9 @@ cd "$condor_dir" || exit
 
 # Iterate over each file in the directory
 for file in *.sub; do
-  # Submit the Condor job
-  condor_submit -spool "$file"
+  # If prefix is provided, check if the file name starts with the prefix
+  if [ -z "$prefix" ] || [[ "$file" == "$prefix"* ]]; then
+    # Submit the Condor job
+    condor_submit -spool "$file"
+  fi
 done
