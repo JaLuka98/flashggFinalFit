e95c2a43964545ebdc616b2f88dda61f0ae9b366
Nico HÃaerringer <n.haerringer@bluewin.ch>
2025-02-23 16:19:59 +0100

    Minor changes.

--- law/run_bootstrap.sh
+++
@@ -1,3 +0,0 @@
-law run Trees2WSData --variable PTH --year 2023 --bootstrap-flag True --number-of-bootstrap 2 --workers 4 --version v1 --workflow local
-
-law run Background --variable PTH --year 2023 --bootstrap-flag True --number-of-bootstrap 2 --workers 4 
