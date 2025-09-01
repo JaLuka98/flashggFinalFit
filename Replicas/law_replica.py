af2e867481756c59011b043b1265306f7c734d05
Nico HÃaerringer <n.haerringer@bluewin.ch>
2025-09-01 15:00:15 +0200

    Use again algo singles in law_replica.py

--- Replicas/law_replica.py
+++ Replicas/law_replica.py
@@ -874,8 +874,8 @@ class FitSplusBToy(Task, SlurmWorkflow, HTCondorWorkflow, law.LocalWorkflow): #(
                 "--X-rtd", "MINIMIZER_multiMin_hideConstants",
                 "--X-rtd", "MINIMIZER_multiMin_maskConstraints",
                 "--X-rtd", "MINIMIZER_multiMin_maskChannels=2",
-                # "--algo", "singles",
-                "--algo", "none",
+                "--algo", "singles",
+                # "--algo", "none", # Bekomme shit korrelierte Parameter zurueck ヽ(｀Д´)ﾉ
                 # "--saveFitResult",
                 "--setParameters", f"""{pdfIdx}""",
                 # "--freezeParameters", f"""MH,{pdfIdx}""",
