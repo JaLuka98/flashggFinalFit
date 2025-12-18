2e77beff6e205ca47774810e399d57889fc60c0e
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2025-12-18 12:09:39 +0100

    Flexibilisation implementation of lumi, suggestion by Max

--- Plots/Spectra/law_spectra.py
+++ Plots/Spectra/law_spectra.py
@@ -430,7 +430,17 @@ class CreateDiffSpectra(law.Task):#(law.Task): #(Task, HTCondorWorkflow, law.Loc
         else:
             cms_label = "Preliminary"
         # print(args.no_preliminary, cms_label)
-        intLumi = plotting_config["lumi"] if "lumi" in plotting_config else lumiMap[f'{self.year}']
+        # Use lumi from config if available, else build it from the individual years if something like 2022_2023 is queried
+        # If that is also not the case, it is a single year, so take lumi from the map
+        if "lumi" in plotting_config:
+            intLumi = plotting_config["lumi"]
+        else:
+            year_str = str(self.year)
+            if "_" in year_str:
+                years = year_str.split("_")
+                intLumi = sum(lumiMap[y] for y in years)
+            else:
+                intLumi = lumiMap[year_str]
         hep.cms.label(cms_label, data=convert_boolean_string(self.is_unblinded), lumi=intLumi, fontsize=20, com=13.6)
 
         # Plot theoretical predictions and experimental data
